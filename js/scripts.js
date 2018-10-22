088f044539e3d6a5cd23a31c51cf8791dd8e64f8
Christabel Escarez <christabel.escarez@gmail.com>
2018-10-22 13:37:45 -0700

    add triangle object array properties to allow sides to be sorted prior to side check

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,19 @@ function Triangle(side1, side2, side3) {
   this.firstSide = side1;
   this.secondSide = side2;
   this.thirdSide = side3;
+  this.sidesToSort = [];
+  this.sortedSides = [];
 }
 
-// Triangle.prototype.checkSides = function(firstSide, secondSide, thirdSide) {
-//   if (firstSide === secondSide === thirdSide) {
-//     alert("Equilateral!");
-//   } else if (firstSide === secondSide !=== thirdSide || firstSide !== secondSide === thirdSide || firstSide === thirdSide !== secondSide) {
-//     alert("Isosceles");
-//   } else if (firstSide !== secondSide !== thirdSide) {
-//     alert("Scalene!");
-//   } else {
-//     alert("Try again");
-//   }
-// }
-
-
-
 Triangle.prototype.checkSides = function(a, b, c) {
-  if (a === b && b === c) {
-    alert("Equilateral!");
-  } else if (a !== b && b !== c && a !== c) {
-    alert("Scalene");
-  } else if (a+b < c || c+b < a || a+c < b) {
-    alert("Try again");
+  this.sidesToSort.push(a,b,c);
+  this.sortedSides = this.sidesToSort.sort();
+  if (this.sortedSides[0] + this.sortedSides[1] < this.sortedSides[2] || this.sortedSides[2]+this.sortedSides[1] < this.sortedSides[0] || this.sortedSides[0]+this.sortedSides[2] < this.sortedSides[1]) {
+    alert("Not a triangle");
+  } else if (this.sortedSides[0] === this.sortedSides[1] && this.sortedSides[1] === this.sortedSides[2]) {
+    alert("Equilateral");
+  } else if (this.sortedSides[0] !== this.sortedSides[1] && this.sortedSides[1] !== this.sortedSides[2] && this.sortedSides[0] !== this.sortedSides[2]) {
+     alert("Scalene");
   } else {
     alert("Isosceles");
   }
@@ -35,11 +25,13 @@ $(document).ready(function() {
   $("form#triangle-sides").submit( function (event) {
     event.preventDefault();
 
+
     var firstSide = parseInt($("#first-side").val());
     var secondSide = parseInt($("#second-side").val());
     var thirdSide = parseInt($("#third-side").val());
     var newTriangle = new Triangle(firstSide, secondSide, thirdSide);
-    newTriangle.checkSides();
+
+    newTriangle.checkSides(firstSide, secondSide, thirdSide);
     console.log(newTriangle);
 
 
